dc10fc378010bc9d6550c63a521fa994a706ce4a
Jiten Palaparthi <JPalaparthi@vmware.com>
2022-06-07 14:42:46 +0530

    Add Variable declarations

---
+++ variables.js
@@ -0,0 +1,39 @@
+x = 100;
+
+console.log(x);
+
+var x1;
+
+console.log(x1);
+
+var x2 = 200;
+
+console.log("Number int=",x2,"  Type of x2", typeof x2);
+
+x2 = 200.45;
+
+console.log("Number Float=",x2,"  Type of x2", typeof x2);
+
+x2 = true;
+
+console.log("Boolean value",x2,"  Type of x2", typeof x2);
+
+x2= "Hello World";
+
+console.log("String Value=",x2,"  Type of x2", typeof x2);
+
+x2 ='I';
+
+console.log("Char Value=",x2,"  Type of x2", typeof x2);
+
+x2 = {"name":"Jiten","gender":"male","pronouns":"he/him","age":37,"isMarried":true};
+
+console.log("Json Object",x2,"  Type of x2", typeof x2);
+
+
+x3 = {"name":"Jiten","gender":"male","pronouns":"he/him","age":37,"isMarried":true};
+
+console.log("Json Object to x3=",x3,"  Type of x2", typeof x2);
+
+
+
